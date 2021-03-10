b2cc3c58f9a25a725d8cabc3c8a2887c7e2a2bc6
Calebe Rodrigues <calebebrodrigues@gmail.com>
2021-03-10 14:06:41 -0300

    Find method

--- src/main/java/aplicacao/Program.java
+++ src/main/java/aplicacao/Program.java
@@ -9,27 +9,19 @@ import dominio.Pessoa;
 public class Program {
 
 	public static void main(String[] args) {
-		Pessoa p1 = new Pessoa(null, "Calebe Rodrigues", "calebe@gmail.com");
-		Pessoa p2 = new Pessoa(null, "Carlos da Silva", "calos@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
-		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		
 		EntityManager em = emf.createEntityManager();
 		
-		// JPA precisa de uma transação ao fazer uma operação != select
-		em.getTransaction().begin();
+		// Método find busca por ID
+		Pessoa pessoa = em.find(Pessoa.class, 2);
 		
-		// Método persist salva no banco de dados
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-		
-		// Confirma as operações
-		em.getTransaction().commit();
-		
+		System.out.println(pessoa);
 		
 		System.out.println("Pronto!");
+		
+		em.close();
+		emf.close();
 	}
 
 }
