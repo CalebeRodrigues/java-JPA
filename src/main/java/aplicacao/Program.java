bca4860c3b75aab75be66d4ca6dd5792f2a7fa95
Calebe Rodrigues <calebebrodrigues@gmail.com>
2021-03-10 14:01:43 -0300

    Inserting elements into the database

--- src/main/java/aplicacao/Program.java
+++ src/main/java/aplicacao/Program.java
@@ -1,17 +1,35 @@
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Pessoa p1 = new Pessoa(1, "Calebe Rodrigues", "calebe@gmail.com");
-		Pessoa p2 = new Pessoa(1, "Carlos da Silva", "calos@gmail.com");
-		Pessoa p3 = new Pessoa(1, "Joaquim Torres", "joaquim@gmail.com");
+		Pessoa p1 = new Pessoa(null, "Calebe Rodrigues", "calebe@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Carlos da Silva", "calos@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
 		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		
+		EntityManager em = emf.createEntityManager();
+		
+		// JPA precisa de uma transação ao fazer uma operação != select
+		em.getTransaction().begin();
+		
+		// Método persist salva no banco de dados
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		
+		// Confirma as operações
+		em.getTransaction().commit();
+		
+		
+		System.out.println("Pronto!");
 	}
 
 }
